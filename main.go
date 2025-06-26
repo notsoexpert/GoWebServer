dfd9eb72d20408dd1378d9c37d06e5e3b7fd1b38
notsoexpert <notsoexpert@gmail.com>
2025-06-26 16:02:49 -0500

    Completed several lessons. Added .gitignore and ignored intermediaries. Added app, api, and admin namespaces. Added visit metric counter.

--- main.go
+++ main.go
@@ -3,18 +3,60 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) CountRequestsHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("Content-Type", "text/html; charset=utf-8")
+	response.Write(fmt.Appendf([]byte{}, `
+	<html>
+		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>
+	`, cfg.fileserverHits.Load()))
+}
+
+func (cfg *apiConfig) ResetRequestsHandler(response http.ResponseWriter, request *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+func ReadinessHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	response.WriteHeader(200)
+	response.Write([]byte("OK"))
+}
+
 func main() {
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /api/healthz", ReadinessHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.CountRequestsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.ResetRequestsHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
+	fmt.Println("Running server...")
 	ok := server.ListenAndServe()
 	if ok != nil {
 		fmt.Println(ok.Error())
 	}
+	fmt.Println("Server shutting down...")
 }
