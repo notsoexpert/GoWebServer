c54418018f0dff4a56505cb48bdab1cb54ed28ea
notsoexpert <notsoexpert@gmail.com>
2025-06-30 11:24:52 -0500

    Didn't actually implement db interfacing with api/chirps. This has been implemented.

--- internal/api/chirps.go
+++ internal/api/chirps.go
@@ -3,9 +3,20 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/notsoexpert/gowebserver/internal/database"
 )
+
+type Chirp struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Body      string    `json:"body,omitempty"`
+	UserID    uuid.UUID `json:"user_id"`
+	Error     string    `json:"error,omitempty"`
+}
 
 func (cfg *APIConfig) ChirpsHandler(response http.ResponseWriter, request *http.Request) {
 	type requestParameters struct {
@@ -26,10 +37,23 @@ func (cfg *APIConfig) ChirpsHandler(response http.ResponseWriter, request *http.
 		return
 	}
 
-	respBody := responseParameters{
+	sqlChirp, err := cfg.DBQueries.PostChirp(request.Context(), database.PostChirpParams{
 		Body:   cleanResponseBody(params.Body),
-		UserID: params.UserID,
+		UserID: uuid.NullUUID{UUID: params.UserID, Valid: true},
+	})
+	if err != nil {
+		respondWithError(response, 400, "Server failed to create chirp record")
+		return
 	}
+
+	respBody := Chirp{
+		ID:        sqlChirp.ID,
+		CreatedAt: sqlChirp.CreatedAt,
+		UpdatedAt: sqlChirp.UpdatedAt,
+		Body:      sqlChirp.Body,
+		UserID:    sqlChirp.UserID.UUID,
+	}
+
 	data, encErr := json.Marshal(respBody)
 	if encErr != nil {
 		respondWithError(response, 500, "Server failed to encode response")
